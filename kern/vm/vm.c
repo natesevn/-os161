10dc3c7521a793e91706780f356ecfa8bd896881
Nicholas Handaja <nikoniko562@gmail.com>
2016-04-14 14:29:00 -0700

    vm_fault semi implemented

--- kern/vm/vm.c
+++ kern/vm/vm.c
@@ -210,82 +210,113 @@ vm_tlbshootdown(const struct tlbshootdown *ts)
 int
 vm_fault(int faulttype, vaddr_t faultaddress)
 {
-    vaddr_t vbase1, vtop1, vbase2, vtop2, stackbase, stacktop;
-    paddr_t paddr;
-    int i;
-    uint32_t ehi, elo;
     struct addrspace *as;
+    struct pagetable_entry *faultentry;
+    vaddr_t vbase, vtop;
+    paddr_t paddr;
+    size_t regionSize; 
+    size_t i;
+    uint32_t ehi, elo;
     int spl;
+    
+    /* Get as from curproc. */
+    as = proc_getas();
+    if (as == NULL) {
+        return EFAULT;
+    }
+    
+    /* Assert that the address space has been set up properly. */
+    KASSERT(as->as_stack_start != 0);
+    KASSERT(as->as_stack_end != 0);
+    KASSERT(as->as_heap_start != 0);
+    KASSERT(as->as_heap_end != 0);
+    KASSERT(as->regionlist != NULL);
+    KASSERT(as->as_pages != NULL);
+    KASSERT((as->as_stack_start & PAGE_FRAME) == as->as_stack_start);
+    KASSERT((as->as_stack_end & PAGE_FRAME) == as->as_stack_end);
+    KASSERT((as->as_heap_start & PAGE_FRAME) == as->as_heap_start);
+    KASSERT((as->as_heap_end & PAGE_FRAME) == as->as_heap_end);
+    //TODO: check vbase, vaddr of pte and regions?
 
     faultaddress &= PAGE_FRAME;
 
     DEBUG(DB_VM, "dumbvm: fault: 0x%x\n", faultaddress);
 
+    /* Determine fault type and act accordingly. */
     switch (faulttype) {
         case VM_FAULT_READONLY:
-        /* We always create pages read-write, so we can't get this */
-        panic("dumbvm: got VM_FAULT_READONLY\n");
+
         case VM_FAULT_READ:
+
         case VM_FAULT_WRITE:
+
         break;
         default:
         return EINVAL;
     }
 
-    if (curproc == NULL) {
-        /*
-         * No process. This is probably a kernel fault early
-         * in boot. Return EFAULT so as to panic instead of
-         * getting into an infinite faulting loop.
-         */
-        return EFAULT;
+    /* Check if the vaddr is valid. */
+    regionSize = sizeof(as->regionlist)/sizeof(as->regionlist[0]);
+    for(i=0; i<regionSize; i++) {
+        vbase = as->regionlist[i].as_vbase;
+        vtop = vbase + as->regionlist[i].as_npages;
+        
+        if(faultaddress >= vbase && faultaddress <vtop) {
+            break;
+        }
+    }
+    
+    /* Return an error if the vaddr is invalid. */
+    //TODO: FIGURE OUT WHICH ERROR
+    if (i == regionSize) {
+        
+        /* Check if its not a stack vaddr. */
+        if (faultaddress < as->as_stack_start || 
+            faultaddress >= as->as_stack_end) {
+            return ERROR;
+        }
+        /* Check if its not a heap vaddr. */
+        else if (faultaddress < as->as_heap_start ||
+                 faultaddress >= as_heap_end) {
+            return ERROR;
+        } 
+        
+    }
+    
+    /* Get pagetable entry. */
+    faultentry = get_pagetable_entry(as, faultaddress);
+   
+    /* PAGE FAULT if NULL. */
+    if (faultentry == NULL) {
+        /* Create new pte for this vaddr. */   
+        //TODO: get permissions from correct region.
+        int permissions = 0;    
+        size_t ptSize = sizeof(as->as_pages)/sizeof(as->as_pages[0]);
+
+        paddr = getppages(1);
+        if(paddr == NULL) {
+            return ENOMEM;
+        }
+
+        /* make sure it's page-aligned */
+        KASSERT((paddr & PAGE_FRAME) == paddr);
+
+        /* Page table is full, so resize. */
+        if(as->as_pages[ptSize-1].pte_vaddr != 0) {
+            as->as_pages = pagetable_resize(as->as_pages, ptSize);
+        }
+        else {
+            ptSize--;
+        }
+        
+        /* Fill in page table entry. */
+        as->as_pages[ptSize].pte_vaddr = faultaddr;
+        as->as_pages[ptSize].pte_paddr = paddr;
+        as->as_pages[ptSize].permissions = permissions;
     }
 
-    as = proc_getas();
-    if (as == NULL) {
-        /*
-         * No address space set up. This is probably also a
-         * kernel fault early in boot.
-         */
-        return EFAULT;
-    }
-
-    /* Assert that the address space has been set up properly. */
-    KASSERT(as->as_vbase1 != 0);
-    KASSERT(as->as_pbase1 != 0);
-    KASSERT(as->as_npages1 != 0);
-    KASSERT(as->as_vbase2 != 0);
-    KASSERT(as->as_pbase2 != 0);
-    KASSERT(as->as_npages2 != 0);
-    KASSERT(as->as_stackpbase != 0);
-    KASSERT((as->as_vbase1 & PAGE_FRAME) == as->as_vbase1);
-    KASSERT((as->as_pbase1 & PAGE_FRAME) == as->as_pbase1);
-    KASSERT((as->as_vbase2 & PAGE_FRAME) == as->as_vbase2);
-    KASSERT((as->as_pbase2 & PAGE_FRAME) == as->as_pbase2);
-    KASSERT((as->as_stackpbase & PAGE_FRAME) == as->as_stackpbase);
-
-    vbase1 = as->as_vbase1;
-    vtop1 = vbase1 + as->as_npages1 * PAGE_SIZE;
-    vbase2 = as->as_vbase2;
-    vtop2 = vbase2 + as->as_npages2 * PAGE_SIZE;
-    stackbase = USERSTACK - DUMBVM_STACKPAGES * PAGE_SIZE;
-    stacktop = USERSTACK;
-
-    if (faultaddress >= vbase1 && faultaddress < vtop1) {
-        paddr = (faultaddress - vbase1) + as->as_pbase1;
-    }
-    else if (faultaddress >= vbase2 && faultaddress < vtop2) {
-        paddr = (faultaddress - vbase2) + as->as_pbase2;
-    }
-    else if (faultaddress >= stackbase && faultaddress < stacktop) {
-        paddr = (faultaddress - stackbase) + as->as_stackpbase;
-    }
-    else {
-        return EFAULT;
-    }
-
-    /* make sure it's page-aligned */
-    KASSERT((paddr & PAGE_FRAME) == paddr);
+    /* Write a valid tlb entry to the TLB table. */
+    
 
     /* Disable interrupts on this CPU while frobbing the TLB. */
     spl = splhigh();
